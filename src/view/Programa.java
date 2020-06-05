7955ab3459daa39eec784e18110380c20a6cd757
Vineee75 <viniciussouza81@yahoo.com.br>
2020-06-05 15:34:53 -0300

    Implementando delete

--- src/view/Programa.java
+++ src/view/Programa.java
@@ -2,6 +2,7 @@ package view;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -11,6 +12,7 @@ import model.entities.Seller;
 public class Programa {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		System.out.println("=== Teste 1: seller findById ===");
@@ -42,6 +44,13 @@ public class Programa {
 		sellerDao.update(seller);
 		System.out.println("Update completo!");
 		
+		System.out.println("\n=== Teste 6: seller delete ===");
+		System.out.print("Entre com o Id pro teste de deleção: ");
+		int id = sc.nextInt();
+		sellerDao.deleteById(id);
+		System.out.println("Deleção completa");
+		
+		sc.close();
 	}
 
 }
