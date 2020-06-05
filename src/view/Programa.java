317817b4f766f890bdd99e7b2a3b2fddb9ea9388
Vineee75 <viniciussouza81@yahoo.com.br>
2020-06-05 11:52:22 -0300

    Classe de Departamento

---
+++ src/view/Programa.java
@@ -0,0 +1,14 @@
+package view;
+
+import model.entities.Department;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		
+		Department obj = new Department(1, "Livros");
+		System.out.println(obj);
+
+	}
+
+}
