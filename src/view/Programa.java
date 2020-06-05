247dfd4e930edb13d38f95b6fe2ad4adfa5661e8
Vineee75 <viniciussouza81@yahoo.com.br>
2020-06-05 15:15:13 -0300

    Implementando insert

--- src/view/Programa.java
+++ src/view/Programa.java
@@ -1,5 +1,6 @@
 package view;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -30,6 +31,10 @@ public class Programa {
 			System.out.println(obj);
 		}
 		
+		System.out.println("\n=== Teste 4: seller insert ===");
+		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+		sellerDao.insert(newSeller);
+		System.out.println("Inserido: novo Id = " + newSeller.getId());
 	}
 
 }
