39c7bbcead33760f25027d11020080001a5789b1
Vineee75 <viniciussouza81@yahoo.com.br>
2020-06-05 12:16:35 -0300

    SellerDaoJDBC e DaoFactory

--- src/view/Programa.java
+++ src/view/Programa.java
@@ -2,6 +2,8 @@ package view;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -11,6 +13,9 @@ public class Programa {
 		
 		Department obj = new Department(1, "Livros");
 		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.00, obj);
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
 		System.out.println(seller);
 
 	}
